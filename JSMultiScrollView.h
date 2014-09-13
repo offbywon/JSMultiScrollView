c495411bd5c5812e364c73912d0401eb042b2743
Jsdodgers <justin@stuckpixelinc.com>
2014-09-13 14:07:57 -0700

    Added multi scroll view.

---
+++ JSMultiScrollView.h
@@ -0,0 +1,19 @@
+//
+//  JSMultiScrollView.h
+//  StackOverflow
+//
+//  Created by Justin Saletta on 9/12/14.
+//  Copyright (c) 2014 jsdodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface JSMultiScrollView : UIScrollView <UIScrollViewDelegate>
+
+- (CGRect)originalFrameForView:(UIView *)view;
+- (CGSize)originalSizeForView:(UIView *)view;
+- (void)setMultiScrolling:(BOOL)multiScrolling forView:(UIView *)view;
+- (void)addSubview:(UIView *)view  multiScrolling:(BOOL)multiScrolling;
+
+@end
---
+++ JSMultiScrollView.m
@@ -0,0 +1,443 @@
+//
+//  JSMultiScrollView.m
+//  StackOverflow
+//
+//  Created by Justin Saletta on 9/12/14.
+//  Copyright (c) 2014 jsdodgers. All rights reserved.
+//
+
+#import "JSMultiScrollView.h"
+
+
+@interface JSScrollSubview : NSObject
+
+@property (nonatomic, strong) UIView *subview;
+//@property (nonatomic) float changedSize;
+@property (nonatomic) CGSize userSetContentSize;
+@property (nonatomic) CGRect userSetFrame;
+@property (nonatomic, strong) JSScrollSubview *previous;
+@property (nonatomic, strong) JSScrollSubview *next;
+@property (nonatomic) BOOL multiScrolls;
+@property (nonatomic, strong) UIPanGestureRecognizer *gesture;
+@property (nonatomic) int shouldSetFrame;
+@property (nonatomic) int shouldSetContentSize;
+
+- (id)initWithSubview:(UIView *)subview;
+
+@end
+
+@implementation JSScrollSubview
+
+- (id)initWithSubview:(UIView *)subview {
+	if ((self = [self init])) {
+		_subview = subview;
+		_shouldSetFrame = 0;
+		_shouldSetContentSize = 0;
+		_userSetFrame = subview.frame;
+		if ([subview respondsToSelector:@selector(contentSize)]) {
+			_userSetContentSize = [[subview valueForKeyPath:NSStringFromSelector(@selector(contentSize))] CGSizeValue];
+		}
+		if ([subview isKindOfClass:[UIScrollView class]]) _multiScrolls = YES;
+	}
+	return self;
+}
+
+@end
+
+@interface JSMultiScrollView ()
+
+@property (nonatomic, strong) UIScrollView *containerScrollView;
+@property (nonatomic, weak) id<UIScrollViewDelegate> realDelegate;
+@property (nonatomic, strong) JSScrollSubview *topMostView;
+@property (nonatomic, strong) JSScrollSubview *bottomMostView;
+@property (nonatomic) CGSize userSetContentSize;
+@property (nonatomic) BOOL drawn;
+
+@end
+
+@implementation JSMultiScrollView
+
+- (id)initWithFrame:(CGRect)frame {
+	if ((self = [super initWithFrame:frame])) {
+		[super setDelegate:self];
+		self.containerScrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
+		[self.containerScrollView setScrollsToTop:NO];
+		[self.containerScrollView setBackgroundColor:[UIColor clearColor]];
+		[self.containerScrollView removeGestureRecognizer:self.containerScrollView.panGestureRecognizer];
+		[self.containerScrollView addGestureRecognizer:self.panGestureRecognizer];
+		[super addSubview:self.containerScrollView];
+	}
+	return self;
+}
+
+- (void)addSubview:(UIView *)view multiScrolling:(BOOL)multiScrolling {
+	[self addSubview:view];
+	[self setMultiScrolling:multiScrolling forView:view];
+}
+
+- (void)addSubview:(UIView *)view {
+	NSString *sourceString = [[NSThread callStackSymbols] objectAtIndex:1];
+	// Example: 1   UIKit                               0x00540c89 -[UIApplication _callInitializationDelegatesForURL:payload:suspended:] + 1163
+	NSCharacterSet *separatorSet = [NSCharacterSet characterSetWithCharactersInString:@" -[]+?.,"];
+	NSMutableArray *array = [NSMutableArray arrayWithArray:[sourceString  componentsSeparatedByCharactersInSet:separatorSet]];
+	[array removeObject:@""];
+	if (![[array objectAtIndex:3] isEqualToString:@"<redacted>"]) {
+		[self.containerScrollView addSubview:view];
+		JSScrollSubview *s = [[JSScrollSubview alloc] initWithSubview:view];
+		if (!self.topMostView) {
+				self.topMostView = s;
+			if (!self.bottomMostView)
+				self.bottomMostView = self.topMostView;
+		}
+		else {
+			JSScrollSubview *s2 = self.topMostView;
+			BOOL inserted = NO;
+			while (s2) {
+				if (view.frame.origin.y < s2.subview.frame.origin.y) {
+					s2.previous.next = s;
+					s.previous = s2.previous;
+					s2.previous = s;
+					s.next = s2;
+					if (s2==self.topMostView) self.topMostView = s;
+					inserted = YES;
+					break;
+				}
+				s2 = s2.next;
+			}
+			if (!inserted) {
+				NSLog(@"Inserted:\n%@\n%@",s.subview,self.bottomMostView.subview);
+				self.bottomMostView.next = s;
+				s.previous = self.bottomMostView;
+				self.bottomMostView = s;
+			}
+		}
+		if ([view isKindOfClass:[UIScrollView class]]) {
+			UIScrollView *sv = (UIScrollView *)view;
+			s.gesture = sv.panGestureRecognizer;
+			[sv removeGestureRecognizer:sv.panGestureRecognizer];
+			[sv setScrollsToTop:NO];
+		}
+		if ([view respondsToSelector:@selector(frame)])
+			[view addObserver:self forKeyPath:NSStringFromSelector(@selector(frame)) options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:NULL];
+		if ([view respondsToSelector:@selector(contentSize)])
+			[view addObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:NULL];
+		if ([view respondsToSelector:@selector(superview)])
+			[view addObserver:self forKeyPath:NSStringFromSelector(@selector(superview)) options:0 context:NULL];
+	}
+	else {
+		[super addSubview:view];
+	}
+	[self setNeedsLayout];
+}
+
+- (void)setDelegate:(id<UIScrollViewDelegate>)delegate {
+	if (delegate!=self)
+		self.realDelegate = delegate;
+	else [super setDelegate:delegate];
+}
+
+- (void)setContentSize:(CGSize)contentSize {
+	[self setContentSize:contentSize userSet:YES];
+}
+
+- (void)setContentSize:(CGSize)contentSize userSet:(BOOL)userSet {
+	[super setContentSize:contentSize];
+	if (userSet) {
+		self.userSetContentSize = contentSize;
+		[self setNeedsLayout];
+	}
+}
+
+
+- (UIScrollView *)containerScrollView {
+	if (!_containerScrollView) _containerScrollView = [[UIScrollView alloc] init];
+	return _containerScrollView;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+//	NSLog(@"Value Changed!!!!:\n%@\n%@\n\n%@",object,keyPath,change);
+	if ([keyPath isEqualToString:NSStringFromSelector(@selector(superview))]) {
+		if ([object superview]!=self.containerScrollView) {
+			JSScrollSubview *s = self.topMostView;
+			while (s) {
+				if (s.subview == object) {
+					s.previous.next = s.next;
+					s.next.previous = s.previous;
+					if (s==self.topMostView) self.topMostView = s.next;
+					if (s==self.bottomMostView) self.bottomMostView = s.previous;
+					s.next = nil;
+					s.previous = nil;
+					break;
+				}
+				s = s.next;
+			}
+			@try {
+				if ([object respondsToSelector:@selector(frame)])
+					[object removeObserver:self forKeyPath:NSStringFromSelector(@selector(frame))];
+				if ([object respondsToSelector:@selector(contentSize)])
+					[object removeObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize))];
+				if ([object respondsToSelector:@selector(superview)])
+					[object removeObserver:self forKeyPath:NSStringFromSelector(@selector(superview))];
+			} @catch (NSException * __unused exception) {}
+		}
+	}
+	else if ([keyPath isEqualToString:NSStringFromSelector(@selector(frame))]) {
+		
+		JSScrollSubview *s = self.topMostView;
+		while (s) {
+			if (s.subview == object) {
+				if (s.shouldSetFrame==0) {
+					CGRect rect = [[change valueForKey:@"new"] CGRectValue];
+					float change = rect.origin.y - s.userSetFrame.origin.y;
+					s.userSetFrame = rect;
+					[self reorderView:s change:change];
+					[self setNeedsLayout];
+				}
+				else {
+					s.shouldSetFrame--;
+				}
+				break;
+			}
+			s = s.next;
+		}
+	}
+	else if ([keyPath isEqualToString:NSStringFromSelector(@selector(contentSize))]) {
+		JSScrollSubview *s = self.topMostView;
+		while (s) {
+			if (s.subview == object) {
+				if (s.shouldSetContentSize==0) {
+					s.userSetContentSize = [[change valueForKey:@"new"] CGSizeValue];
+					[self setNeedsLayout];
+				}
+				else {
+					s.shouldSetContentSize--;
+				}
+				break;
+			}
+			s = s.next;
+		}
+	}
+}
+
+- (void)reorderView:(JSScrollSubview *)sView change:(float)change {
+	NSLog(@"Reorder View:\n%@",sView.subview);
+	JSScrollSubview *s = sView;
+	while ((s = (change>0?sView.next:sView.previous))) {
+		if (change>0 &&sView.userSetFrame.origin.y>s.userSetFrame.origin.y) {
+			s.previous = sView.previous;
+			sView.next = s.next;
+			s.next = sView;
+			sView.previous = s;
+			if (s==self.bottomMostView) self.bottomMostView = sView;
+		}
+		else if (change < 0 && sView.userSetFrame.origin.y<s.userSetFrame.origin.y) {
+			s.next = sView.next;
+			sView.previous = s.previous;
+			s.previous = sView;
+			sView.next = s;
+			if (s==self.topMostView) self.topMostView = sView;
+		}
+		else {
+			break;
+		}
+	}
+}
+
+- (float)scrollingViewSizes {
+	float size = 0.0f;
+	JSScrollSubview *s = self.topMostView;
+	while (s) {
+		if ([s isKindOfClass:[UIScrollView class]] && s.multiScrolls) {
+			UIScrollView *scroll = (UIScrollView *)s.subview;
+			size += scroll.contentSize.height - scroll.frame.size.height;
+		}
+		s = s.next;
+	}
+	return size;
+}
+
+- (void)setMultiScrolling:(BOOL)multiScrolling forView:(UIView *)view {
+	JSScrollSubview *s = self.topMostView;
+	while (s) {
+		if (s.subview == view) {
+			if (multiScrolling && !s.multiScrolls) {
+				if (((UIScrollView *)s.subview).panGestureRecognizer)
+					s.gesture = ((UIScrollView *)s.subview).panGestureRecognizer;
+			}
+			else if (!multiScrolling && s.multiScrolls) {
+				if (s.gesture)
+					[((UIScrollView *)s.subview) addGestureRecognizer:s.gesture];
+			}
+
+			[s setMultiScrolls:multiScrolling];
+			return;
+		}
+		s = s.next;
+	}
+}
+
+- (CGRect)originalFrameForView:(UIView *)view {
+	JSScrollSubview *s = self.topMostView;
+	while (s) {
+		if (s.subview == view) return s.userSetFrame;
+		s = s.next;
+	}
+	return CGRectZero;
+}
+
+- (CGSize)originalSizeForView:(UIView *)view {
+	JSScrollSubview *s = self.topMostView;
+	while (s) {
+		if (s.subview == view) return s.userSetContentSize;
+		s = s.next;
+	}
+	return CGSizeZero;
+}
+
+- (void)layoutSubviews {
+	[super layoutSubviews];
+	[UIView animateKeyframesWithDuration:(!self.drawn?0.0f:0.3) delay:0.0 options:UIViewKeyframeAnimationOptionBeginFromCurrentState animations:^() {
+		float change = 0.0f;
+		float contentSizeChange = 0.0f;
+		JSScrollSubview *s = self.topMostView;
+		while (s) {
+			UIView *v = s.subview;
+			//	CGRect fr = v.frame;
+			//	fr.origin.y += 15;
+			//	if (fr.origin.y >=500) fr.origin.y = 50.0f;
+			CGRect fr = s.userSetFrame;
+			fr.origin.y+=change;
+			if ([v isKindOfClass:[UIScrollView class]] && s.multiScrolls) {
+				UIScrollView *sv = (UIScrollView *)v;
+				fr.size.height = MIN(self.frame.size.height,sv.contentSize.height);//s.userSetContentSize.height);//sv.contentSize.height);
+				change += fr.size.height - s.userSetFrame.size.height;
+				contentSizeChange += sv.contentSize.height - fr.size.height;
+			}
+			if (!CGRectEqualToRect(fr, v.frame)) {
+				s.shouldSetFrame++;
+				[v setFrame:fr];
+			}
+			s = s.next;
+		}
+		CGSize cs = self.userSetContentSize;
+		cs.height += change + contentSizeChange;
+		[self setContentSize:cs userSet:NO];
+		NSLog(@"Layout: %@",NSStringFromCGSize(cs));
+		NSLog(@"%@ - %@ - %@ -- %.0f",NSStringFromCGRect(self.frame),NSStringFromCGSize(self.contentSize),NSStringFromCGSize(self.userSetContentSize),change);
+		self.drawn = YES;
+	} completion:^(BOOL finished) {
+		
+	}];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+	NSLog(@"ScrollViewDidScroll");
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewDidScroll:)])
+		[self.realDelegate scrollViewDidScroll:scrollView];
+	
+	
+	float offset = scrollView.contentOffset.y;
+	float originalOffset = offset;
+	JSScrollSubview *s = self.topMostView;
+	while (s) {
+//		if (s.subview.frame.origin.y >= offset) break;
+		if (![s.subview isKindOfClass:[UIScrollView class]] || !s.multiScrolls) {
+			s = s.next;
+			continue;
+		}
+		UIScrollView *scroll = (UIScrollView *)s.subview;
+		float diff = MAX(0,MIN(offset - scroll.frame.origin.y, (scroll.contentSize.height - scroll.frame.size.height)));
+		[scroll setContentOffset:CGPointMake(scroll.contentOffset.x, diff)];
+		offset -= diff;
+		s = s.next;
+	}
+	NSLog(@"Scroll offset:%.0f  - %.0f",offset,originalOffset);
+	CGRect fr = self.containerScrollView.frame;
+	fr.origin.y = originalOffset;
+	[self.containerScrollView setFrame:fr];
+	[self.containerScrollView setContentOffset:CGPointMake(self.containerScrollView.contentOffset.x, offset)];
+	
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewWillBeginDragging:)])
+		[self.realDelegate scrollViewWillBeginDragging:scrollView];
+}
+
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewWillEndDragging:withVelocity:targetContentOffset:)])
+		[self.realDelegate scrollViewWillEndDragging:scrollView withVelocity:velocity targetContentOffset:targetContentOffset];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewDidEndDragging:willDecelerate:)])
+		[self.realDelegate scrollViewDidEndDragging:scrollView willDecelerate:decelerate];
+}
+
+- (BOOL)scrollViewShouldScrollToTop:(UIScrollView *)scrollView {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewShouldScrollToTop:)])
+		return [self.realDelegate scrollViewShouldScrollToTop:scrollView];
+	return YES;
+}
+
+- (void)scrollViewDidScrollToTop:(UIScrollView *)scrollView {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewDidScrollToTop:)])
+		[self.realDelegate scrollViewDidScrollToTop:scrollView];
+}
+
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewWillBeginDecelerating:)])
+		[self.realDelegate scrollViewWillBeginDecelerating:scrollView];
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewDidEndDecelerating:)])
+		[self.realDelegate scrollViewDidEndDecelerating:scrollView];
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(viewForZoomingInScrollView:)])
+		return [self.realDelegate viewForZoomingInScrollView:scrollView];
+	return nil;
+}
+
+- (void)scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(UIView *)view {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewWillBeginZooming:withView:)])
+		[self.realDelegate scrollViewWillBeginZooming:scrollView withView:view];
+}
+
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewDidEndZooming:withView:atScale:)])
+		[self.realDelegate scrollViewDidEndZooming:scrollView withView:view atScale:scale];
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewDidZoom:)])
+		[self.realDelegate scrollViewDidZoom:scrollView];
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
+	if (self.realDelegate && [self.realDelegate respondsToSelector:@selector(scrollViewDidEndScrollingAnimation:)])
+		[self.realDelegate scrollViewDidEndScrollingAnimation:scrollView];
+}
+
+
+- (void)dealloc {
+	JSScrollSubview *s = self.topMostView;
+	while (s) {
+		@try {
+			UIView *view = s.subview;
+			if ([view respondsToSelector:@selector(frame)])
+				[view removeObserver:self forKeyPath:NSStringFromSelector(@selector(frame))];
+			if ([view respondsToSelector:@selector(contentSize)])
+				[view removeObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize))];
+			if ([view respondsToSelector:@selector(superview)])
+				[view removeObserver:self forKeyPath:NSStringFromSelector(@selector(superview))];
+			s = s.next;
+		} @catch (NSException * __unused exception) {}
+		
+	}
+}
+
+
+@end
